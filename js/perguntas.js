dda20d39742a9e1685b190e407e319e44ba16ae3
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-08-15 13:29:15 -0300

    g

--- js/perguntas.js
+++ js/perguntas.js
@@ -6,7 +6,7 @@ export const perguntas = [
                 texto: "Ele resolve seguir sozinho!",
                 afirmacao: [
                     "Você preferiu não ter ajuda para começar sua jornada.",
-                    "Você ficou solitário, mas continuou sua jornada apenas com a sua própria companhia."
+                    "Você ficou solitário, mas continuou sua jornada apenas com a sua própria companhia.",
                 ],
                 proxima: 1,
             },
@@ -14,7 +14,7 @@ export const perguntas = [
                 texto: "O pescador ajuda ele!",
                 afirmacao: [
                     "Você começou sua jornada com ajuda da amizade.",
-                    "Com ajuda tudo ficou mais fácil para você."
+                    "Com ajuda tudo ficou mais fácil para você.",
                 ],
                 proxima: 2,
             },
@@ -27,7 +27,7 @@ export const perguntas = [
                 texto: "Enfrentar os gigantes e lutar contra eles.",
                 afirmacao: [
                     "Você decidiu enfrentar os gigantes diretamente, mesmo sabendo que seria perigoso.",
-                    "Você percebe que a coragem pode ser a chave para superar desafios aparentemente intransponíveis."
+                    "Você percebe que a coragem pode ser a chave para superar desafios aparentemente intransponíveis.",
                 ],
                 proxima: 9,
             },
@@ -35,7 +35,7 @@ export const perguntas = [
                 texto: "Tentar escapar e evitar os gigantes.",
                 afirmacao: [
                     "Você escolheu tentar evitar o confronto com os gigantes.",
-                    "Você percebe que, às vezes, evitar um perigo poderia ser uma escolha mais sensata."
+                    "Você percebe que, às vezes, evitar um perigo poderia ser uma escolha mais sensata.",
                 ],
                 proxima: 9,
             },
@@ -49,7 +49,7 @@ export const perguntas = [
                 afirmacao: [
                     "Você estava atrás de um pote de ouro com o seu amigo .",
                     "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos.",
                 ],
                 proxima: 3,
             },
@@ -71,7 +71,7 @@ export const perguntas = [
                 afirmacao: [
                     "Você nunca teve muitas riquezas com sua família, então foi atrás de ouro para tentar viver melhor.",
                     "Você sempre pensou como seria ter muito dinheiro para ajudar sua família, então decidiu ir atrás do ouro para viver em paz.",
-                    "Você sempre quis muito dinheiro para viver uma vida boa, sem ter sua família, decidiu se arriscar e ir atrás de ouro para viver bem."
+                    "Você sempre quis muito dinheiro para viver uma vida boa, sem ter sua família, decidiu se arriscar e ir atrás de ouro para viver bem.",
                 ],
                 proxima: 4,
             },
@@ -80,7 +80,7 @@ export const perguntas = [
                 afirmacao: [
                     "Você nunca ligou muito para dinheiro, então escolheu ir atrás de chocolates para ficar feliz.",
                     "Chocolate sempre foi seu ponto fraco, então você preferiu ir atrás dele para sua felicidade.",
-                    "Você sempre amou chocolate então foi atrás disso para ter sua felicidade."
+                    "Você sempre amou chocolate então foi atrás disso para ter sua felicidade.",
                 ],
                 proxima: 4,
             },
@@ -93,7 +93,7 @@ export const perguntas = [
                 texto: "Você encontra um pote cheio de ouro e se alegra muito.",
                 afirmacao: [
                     "Você fica feliz por ter encontrado o ouro que procurava.",
-                    "Você sente que todo o esforço e a jornada valeram a pena."
+                    "Você sente que todo o esforço e a jornada valeram a pena.",
                 ],
                 proxima: 5,
             },
@@ -101,7 +101,7 @@ export const perguntas = [
                 texto: "Você encontra um pote cheio de chocolates e fica surpreso vendo todo aquele chocolate.",
                 afirmacao: [
                     "Você se alegra por finalmente encontrar algo que te faz feliz.",
-                    "Você decide aproveitar o momento e celebrar com seus chocolates.."
+                    "Você decide aproveitar o momento e celebrar com seus chocolates.",
                 ],
                 proxima: 5,
             },
@@ -114,7 +114,7 @@ export const perguntas = [
                 texto: "Usar o ouro para reconstruir sua vila longe em paz e ajudar outras pessoas.",
                 afirmacao: [
                     "Você decide usar o ouro para ajudar sua vila e reconstruir o que foi perdido.",
-                    "Você sente que ajudando os outros traz um grande propósito à sua vida."
+                    "Você sente que ajudando os outros traz um grande propósito à sua vida.",
                 ],
                 proxima: 6,
             },
@@ -122,7 +122,7 @@ export const perguntas = [
                 texto: "Usar os chocolates para fazer uma grande festa e compartilhar com todos.",
                 afirmacao: [
                     "Você decide usar os chocolates para fazer uma festa e fazer novos amigos.",
-                    "Você sente que a amizade e a alegria são as verdadeiras recompensas da jornada."
+                    "Você sente que a amizade e a alegria são as verdadeiras recompensas da jornada.",
                 ],
                 proxima: 7,
             },
@@ -135,7 +135,7 @@ export const perguntas = [
                 texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
                     "Você sente que cumpriu sua missão e fez uma grande diferença na vida das pessoas.",
-                    "Você percebe que a verdadeira felicidade está em ajudar os outros e fazer o bem."
+                    "Você percebe que a verdadeira felicidade está em ajudar os outros e fazer o bem.",
                 ],
                 proxima: 8,
             },
@@ -143,7 +143,7 @@ export const perguntas = [
                 texto: "Você se sente um pouco desapontado, pois esperava algo mais.",
                 afirmacao: [
                     "Você percebe que a reconstrução trouxe alegria, mas ainda sente uma falta interna.",
-                    "Você começa a refletir sobre o que realmente busca na vida e o que é importante para você."
+                    "Você começa a refletir sobre o que realmente busca na vida e o que é importante para você.",
                 ],
                 proxima: 8,
             },
@@ -156,7 +156,7 @@ export const perguntas = [
                 texto: "Você se sente feliz e satisfeito ao ver todos se divertindo.",
                 afirmacao: [
                     "Você se alegra ao ver a felicidade e a diversão das pessoas ao seu redor.",
-                    "Você sente que fez a escolha certa ao compartilhar a alegria com outros."
+                    "Você sente que fez a escolha certa ao compartilhar a alegria com outros.",
                 ],
                 proxima: 8,
             },
@@ -164,7 +164,7 @@ export const perguntas = [
                 texto: "Você sente que a festa não preencheu o vazio que você sentia.",
                 afirmacao: [
                     "Você percebe que a felicidade temporária não substitui a necessidade de algo mais profundo.",
-                    "Você começa a refletir sobre o que realmente deseja alcançar na vida."
+                    "Você começa a refletir sobre o que realmente deseja alcançar na vida.",
                 ],
                 proxima: 8,
             },
@@ -177,7 +177,7 @@ export const perguntas = [
                 texto: "Aprendeu que a verdadeira riqueza está em ajudar os outros e compartilhar.",
                 afirmacao: [
                     "Você percebe que, embora tenha alcançado seus objetivos, a verdadeira felicidade vem de fazer o bem.",
-                    "Você entende que sua jornada trouxe mais valor para os outros do que para si mesmo."
+                    "Você entende que sua jornada trouxe mais valor para os outros do que para si mesmo.",
                 ],
             },
             {
@@ -196,7 +196,7 @@ export const perguntas = [
                 texto: "Tentar fugir sem que eles percebram.",
                 afirmacao: [
                     "Você tentou fugir mas eles te pegaram mesmo assim, você ficou preso com eles para sempre.",
-                    "Você não conseguiu fugir deles e ficou la pela eternidade."
+                    "Você não conseguiu fugir deles e ficou la pela eternidade.",
                 ],
             },
             {
